06b54ba8e3ed78973fdb46126b4f4fc52f0bd45b
casept <davids.paskevics@gmail.com>
2025-02-08 23:17:32 +0100

    Use rsync to download rootfs without manual compression step

--- download-boot-asteroid.sh
+++ download-boot-asteroid.sh
@@ -9,16 +9,10 @@ ROOTIMG="$SCRIPT_PATH/../rootfs-asteroid.img"
 
 # Copy artifacts
 echo "Downloading kernel image..."
-scp "$BUILDSERVER:$BUILDDIR/zImage-exynos3250-rinato.dtb.bin" "$BOOTIMG"
+rsync -zaPL "$BUILDSERVER:$BUILDDIR/zImage-rinato.bin" "$BOOTIMG"
 
-echo "Compressing rootfs..."
-ssh "$BUILDSERVER" zstd -f "$BUILDDIR/asteroid-image-rinato.rootfs.ext4"
 echo "Downloading rootfs..."
-scp "$BUILDSERVER:$BUILDDIR/asteroid-image-rinato.rootfs.ext4.zst" "$ROOTIMG.zst"
-ssh "$BUILDSERVER" rm "$BUILDDIR/asteroid-image-rinato.rootfs.ext4.zst"
-echo "Unpacking rootfs..."
-unzstd -f "$ROOTIMG.zst"
-rm "$ROOTIMG.zst"
+rsync -zaPL "$BUILDSERVER:$BUILDDIR/asteroid-image-rinato.rootfs.ext4" "$ROOTIMG"
 
 # Flash (via Heimdall)
 echo "Flashing kernel..."
